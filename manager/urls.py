1a14b320f86386cba987897a611b03808ef9b9db
Alex <moleksii@gmail.com>
2024-01-03 14:18:59 +0200

    Create necessary urls and views for all models(Create, Update, Delete)

--- manager/urls.py
+++ manager/urls.py
@@ -8,7 +8,17 @@ from manager.views import (
     TaskListView,
     WorkerDetailView,
     PositionListView,
-    TaskTypeCreateView
+    TaskTypeCreateView,
+    PositionCreateView,
+    TaskCreateView,
+    WorkerCreateView,
+    PositionUpdateView,
+    TaskUpdateView,
+    TaskTypeUpdateView,
+    WorkerUpdateView,
+    TaskDeleteView,
+    PositionDeleteView,
+    TaskTypeDeleteView
 )
 
 app_name = "manager"
@@ -25,13 +35,31 @@ urlpatterns = [
         TaskTypeCreateView.as_view(),
         name="task-type-create"
     ),
+    path(
+        "task-types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update"
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete"
+    ),
     path("positions/", PositionListView.as_view(), name="position-list"),
+    path("positions/create/", PositionCreateView.as_view(), name="position-create"),
+    path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
+    path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path(
         "workers/<int:pk>/",
         WorkerDetailView.as_view(),
         name="worker-detail"
     ),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
 ]
--- manager/views.py
+++ manager/views.py
@@ -41,10 +41,43 @@ class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "manager/tasktype_form.html"
 
 
+class TaskTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-type-list")
+    template_name = "manager/tasktype_form.html"
+
+
+class TaskTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = TaskType
+    template_name = "manager/tasktype_confirm_delete.html"
+    success_url = reverse_lazy("manager:task-type-list")
+
+
 class PositionListView(LoginRequiredMixin, generic.ListView):
     model = Position
     queryset = Position.objects.order_by("name")
     paginate_by = 5
+
+
+class PositionCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("manager:position-list")
+    template_name = "manager/position_form.html"
+
+
+class PositionUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("manager:position-list")
+    template_name = "manager/position_form.html"
+
+
+class PositionDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Position
+    template_name = "manager/position_confirm_delete.html"
+    success_url = reverse_lazy("manager:position-list")
 
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
@@ -57,6 +90,26 @@ class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
 
 
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-list")
+    template_name = "manager/task_form.html"
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-list")
+    template_name = "manager/task_form.html"
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    template_name = "manager/task_confirm_delete.html"
+    success_url = reverse_lazy("manager:task-list")
+
+
 class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
     paginate_by = 5
@@ -65,3 +118,17 @@ class WorkerListView(LoginRequiredMixin, generic.ListView):
 class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
     model = Worker
     queryset = Worker.objects.prefetch_related("tasks__assignees")
+
+
+class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Worker
+    fields = "__all__"
+    success_url = reverse_lazy("manager:worker-list")
+    template_name = "manager/worker_form.html"
+
+
+class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Worker
+    fields = "__all__"
+    success_url = reverse_lazy("manager:worker-list")
+    template_name = "manager/worker_form.html"
