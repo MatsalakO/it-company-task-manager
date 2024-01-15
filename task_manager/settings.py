38e7ee4e3993bb52ece15dc43ce89302d8567d92
Alex <moleksii@gmail.com>
2024-01-15 22:47:10 +0200

    Change allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["task-manager-b0me.onrender.com"]
 
 # Application definition
 
